c362745845a8c276405eb7bd7ffa2388244b13f2
berger <robertberger.dev@gmail.com>
2023-02-20 09:44:10 -0500

    updated the App.js with example code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import Stack from 'react-bootstrap/Stack'
+import ContactList from './ContactList'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack gap={3} className="col-md-10 mx-auto">
+      <ContactList />
+    </Stack>
   );
 }
 
